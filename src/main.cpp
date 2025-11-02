3a9bd90c3de4b08383aca8000bf0e35f79b5f4f8
rajmanna <raj2021manna@gmail.com>
2025-11-02 08:43:01 +0000

    complete ez5

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::vector<std::string> builtins{"type", "exit", "echo"};
+
   std::cout << "$ ";
 
   std::string command;
   std::getline(std::cin, command);
-  if(command == "exit 0") {
+  if (command == "exit 0") {
     return 0;
   }
-  if(command.starts_with("echo ")) {
+  if (command.starts_with("echo")) {
     std::cout << command.substr(5) << std::endl;
-  }
-  else {
+  } else if (command.starts_with("type")) {
+    std::string arg = command.substr(5);
+    if (std::find(builtins.begin(), builtins.end(), arg) != builtins.end()) {
+      std::cout << arg << " is a shell builtin" << std::endl;
+    } else {
+      std::cout << arg << ": not found" << std::endl;
+    }
+  } else {
     std::cout << command << ": command not found" << std::endl;
   }
 
