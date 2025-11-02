2b6960180ea457ec1cc05662077c9051a20f538f
rajmanna <raj2021manna@gmail.com>
2025-11-02 08:04:18 +0000

    complete pn5

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ int main() {
 
   std::string command;
   std::getline(std::cin, command);
+  if(command == "exit 0") {
+    return 0;
+  }
   std::cout << command << ": command not found" << std::endl;
 
   main();
