0bbc8b8b44681e1a2433ae1eecff1343b309f174
dev-rajm <dev.rajm8923@gmail.com>
2025-11-06 20:38:11 +0530

    complete #yt5

---
+++ headers/utils.h
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+namespace fs = std::filesystem;
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#ifdef _WIN32
+#include <direct.h>
+#define CHDIR _chdir
+const char DELIMITER = ';';
+#else
+#include <unistd.h>
+#define CHDIR chdir
+const char DELIMITER = ':';
+#endif
+
+std::vector<std::string> split(std::string input, char divider = DELIMITER) {
+  std::vector<std::string> chunks;
+  std::string chunk;
+  std::stringstream ss(input);
+
+  while (std::getline(ss, chunk, divider)) {
+    chunks.emplace_back(chunk);
+  }
+
+  return chunks;
+}
+
+std::vector<std::string> split_input(const std::string &str) {
+  std::vector<std::string> tokens;
+
+  bool in_single_quotes = false;
+  bool in_double_quotes = false;
+  bool in_escape_literal = false;
+  std::string curr_token = "";
+
+  for (auto ch : str) {
+    if (ch == '"' && !in_single_quotes && !in_escape_literal) {
+      in_double_quotes = !in_double_quotes;
+    } else if (ch == '\'' && !in_double_quotes && !in_escape_literal) {
+      in_single_quotes = !in_single_quotes;
+    } else if (ch == '\\' && !in_escape_literal && !in_single_quotes) {
+      in_escape_literal = !in_escape_literal;
+    } else if (std::isspace(ch) && !in_escape_literal && !in_single_quotes &&
+               !in_double_quotes) {
+      if (!curr_token.empty()) {
+        tokens.push_back(curr_token);
+        curr_token.clear();
+      }
+    } else {
+      curr_token += ch;
+      if (in_escape_literal) {
+        in_escape_literal = false;
+      }
+    }
+  }
+
+  if (!curr_token.empty()) {
+    tokens.push_back(curr_token);
+  }
+
+  return tokens;
+}
+
+bool find_executable_path(const std::string exec_path) {
+  if (fs::exists(exec_path)) {
+    fs::file_status exec_status = fs::status(exec_path);
+    fs::perms exec_perm = exec_status.permissions();
+    if ((exec_perm & fs::perms::owner_exec) != fs::perms::none) {
+      return true;
+    }
+  }
+  return false;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,18 @@
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
-#include <regex>
-#include <sstream>
 #include <string>
 #include <vector>
-namespace fs = std::filesystem;
-
-#ifdef _WIN32
-#include <direct.h>
-#define CHDIR _chdir
-const char DELIMITER = ';';
-#else
-#include <unistd.h>
-#define CHDIR chdir
-const char DELIMITER = ':';
-#endif
-
-std::vector<std::string> split(std::string input, char divider = DELIMITER) {
-  std::vector<std::string> chunks;
-  std::string chunk;
-  std::stringstream ss(input);
-
-  while (std::getline(ss, chunk, divider)) {
-    chunks.emplace_back(chunk);
-  }
-
-  return chunks;
-}
-
-bool find_executable_path(const std::string exec_path) {
-  if (fs::exists(exec_path)) {
-    fs::file_status exec_status = fs::status(exec_path);
-    fs::perms exec_perm = exec_status.permissions();
-    if ((exec_perm & fs::perms::owner_exec) != fs::perms::none) {
-      return true;
-    }
-  }
-  return false;
-}
+#include "../headers/utils.h"
 
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // get the path environment variable
+  std::vector<std::string> builtins{"type", "exit", "echo", "pwd"};
+  
   char *process_env = getenv("PATH");
   std::vector<std::string> paths = split(process_env);
-
-  std::vector<std::string> builtins{"type", "exit", "echo", "pwd"};
 
   std::cout << "$ ";
 
@@ -59,7 +23,14 @@ int main() {
   }
 
   if (command.starts_with("echo")) {
-    std::system(command.c_str());
+    std::vector<std::string> str = split_input(command.substr(5));
+    for(size_t i=0; i<str.size(); i++) {
+      std::cout << str[i];
+      if(i+1 < str.size()) {
+        std::cout << " ";
+      }
+    }
+    std::cout << std::endl;
   } else if (command.starts_with("type")) {
     std::string arg = command.substr(5);
     if (std::find(builtins.begin(), builtins.end(), arg) != builtins.end()) {
